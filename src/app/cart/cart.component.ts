1244b073f862741472f62a678f2fb3772986ea11
RizaKaba <rizakaba@gmx.de>
2019-06-06 09:34:29 +0200

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -22,6 +22,14 @@ checkoutForm;
     });
      }
 
+  onSubmit(customerData) {
+    // Process checkout data here
+    console.warn('Your order has been submitted', customerData);
+ 
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
   ngOnInit() {
   }
 
